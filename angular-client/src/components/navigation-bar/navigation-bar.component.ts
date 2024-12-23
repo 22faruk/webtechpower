8e4086e3eb1bb2c969051bdeca12fbb1f77eaf3b
lukas <lukasmu009@gmail.com>
2024-12-23 14:38:49 +0100

    Navigation-bar updated

--- angular-client/src/components/navigation-bar/navigation-bar.component.ts
+++ angular-client/src/components/navigation-bar/navigation-bar.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 
 // Custom modules
 import { SharedAntDesignModule } from '../../module/shared-ant-design/shared-ant-design.module';
+import {NzTypographyComponent} from 'ng-zorro-antd/typography';
+import {NzMenuDirective} from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [SharedAntDesignModule],
+  imports: [SharedAntDesignModule, NzTypographyComponent, NzMenuDirective],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.css',
 })
