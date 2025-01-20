f772ee333780f6ef709c34ea94784864273f52e6
Onur <onur.ayvaz@stud.uni-due.de>
2025-01-20 11:17:48 +0100

    corrections regarding coding guidelines

--- server/src/controllers/quiz-controllers.js
+++ server/src/controllers/quiz-controllers.js
@@ -4,54 +4,56 @@ const User = require("../models/user-model")
 const Flashcard = require("../models/flashcard-model");
 
 //generates a random integer between [from,to)
-function randomNumber(from, to)
-{
+function randomNumber(from, to) {
     return Math.trunc(Math.random() * (to - from) + from)
 }
 
-/*
-    Hinweise:
-        - Jeder User kann nur ein offenes Quiz haben
-                - Erstellt ein User ein neues Quiz, obwohl er bereits eines offen hat, wird dieses einfach überschrieben
-        - Methode createQuiz:
-            - Ist im param nur die subject ID vorhanden, dann wird ein Quiz erstellt, mit allen Karten (aus allen Directories, in diesem Subject)
-            - Wird nach der subject ID zusätzlich eine directory ID übergeben(optional), enthält das Quiz nur die Karten aus dieser Directory
-        - Methode nextQuestion:
-            - Gibt aus dem offenen Quiz, eines Users, eine zufällige Karte zurück (diese wird anschliessend aus dem Quiz vollständig entfernt)
-            - Es muss nichts übergeben werden (user ID wird automatisch aus der req gezogen; Alternativ kann die user ID im Body, in einem Object "user" unter der property "_id" übergeben werden)
-        - Methode numRemainingQuestions:
-            - Gibt zurück, wie viele Fragen noch übrig sind
-            - Es muss nichts übergeben werden
-*/
+async function fetchEntry(schema, id, requiredProperties = []) {
+    const entry = await schema.findById(id);
+
+    if(!entry) {
+        const err = new Error(`${schema.modelName} has no id: ${id}`);
+        err.status = 404;
+        throw err;
+    }
+
+    for(const property of requiredProperties) {
+        if(!entry[property]) {
+            throw new Error(`${schema.modelName} with id: ${id} has no property ${property}`);
+        }
+    }
+
+    return entry;
+}
 
 exports.createQuiz = async(req, res, next) => {
     try {
         const {subjectId, directoryId} = req.params;
-        //const userId = req.user._id;
-
-        const subject = await Subject.findById(subjectId);
-        if(!subject) throw new Error(`No subject exists with id: ${subjectId}!`);
-
-        if(!subject.owner) throw new Error(`Subject with id: ${subjectId} has no owner!`);
+        const subject = await fetchEntry(Subject, subjectId, ["owner"]);
         const userId = subject.owner._id;
-
-        const user = await User.findById(userId);
-        if(!user) throw new Error(`No user exists with id: ${userId}!`);
+        if(!User.findById(userId)) {
+            throw new Error(`User with id: ${userId} not found!`);
+        }
 
         let flashcards = [];
+
         if(!directoryId) {
-            for(const directory of subject.directories)
-            {
+            for(const directory of subject.directories) {
                 flashcards = flashcards.concat(directory.flashcards);
             }
         }
+
         else {
             const directory = subject.directories.find((dir,_) => dir._id == directoryId);
-            if(!directory) throw new Error(`No directory exists with id: ${directoryId}!`);
+            if(!directory) {
+                throw new Error(`No directory exists with id: ${directoryId}!`);
+            }
+
             flashcards = directory.flashcards;
         }
 
-        const quiz = await Quiz.findOneAndUpdate({owner:userId},{flashcards:flashcards,numRemainingFlashcards:flashcards.length},{new:true, upsert:true}).populate({path: 'flashcards', select: 'question answer'});
+        const quiz = await Quiz.findOneAndUpdate({owner:userId},{flashcards:flashcards,numRemainingFlashcards:flashcards.length},{new:true, upsert:true})
+            .populate({path: 'flashcards', select: 'question answer'});
 
         return res.status(200).json({
             message: "Successfully created Quiz",
@@ -66,15 +68,22 @@ exports.createQuiz = async(req, res, next) => {
 
 exports.nextQuestion = async(req,res,next) => {
     try {
-        const userId = (req.user ? req.user._id : (req.body.user ? req.body.user._id : undefined));
-        if(!userId) throw new Error("No user id provided!");
+        //const userId = (req.user ? req.user._id : (req.body.user ? req.body.user._id : undefined));
+        //if(!userId) throw new Error("No user id provided!");
 
-        if(!await User.findById(userId)) throw new Error(`No user exists with id: ${userId}`);
+        const userId = req.user._id;
+        if(!await User.findById(userId)) {
+            throw new Error(`No user exists with id: ${userId}`);
+        }
     
         const quiz = await Quiz.findOne({owner: userId}).populate({path: 'flashcards', select: 'question answer'});
-        if(!quiz) throw new Error(`User with id: ${userId} has no quiz!`);
+        if(!quiz) {
+            throw new Error(`User with id: ${userId} has no quiz!`);
+        }
 
-        if(0 == quiz.numRemainingFlashcards) throw new Error(`No more questions available!`);
+        if(0 == quiz.numRemainingFlashcards) {
+            throw new Error(`No more questions available!`);
+        }
         
         const flashcards = quiz.flashcards;
         const numAnswers = 4;
@@ -86,7 +95,7 @@ exports.nextQuestion = async(req,res,next) => {
                 const randomIndex = randomNumber(0, quiz.numRemainingFlashcards);
                 const flashcard = flashcards[randomIndex];
 
-                //swaps randomly choosen flashcard with the last flashcard in the array (so the remaining flashcards will always be in the range [0,numRemainingFlashcards))
+                //swaps the randomly choosen flashcard with the last flashcard in the array (so remaining flashcards will always be in the range [0,numRemainingFlashcards))
                 const tmp = flashcards[quiz.numRemainingFlashcards - 1];
                 flashcards[randomIndex] = tmp;
                 flashcards[quiz.numRemainingFlashcards - 1] = flashcard;
@@ -100,6 +109,7 @@ exports.nextQuestion = async(req,res,next) => {
             else {
                 const randomIndex = randomNumber(0, flashcards.length);
                 const flashcard = flashcards[randomIndex];
+
                 questionAnswers.answers.push(flashcard.answer);
             }
         }
@@ -119,16 +129,21 @@ exports.nextQuestion = async(req,res,next) => {
 
 exports.numRemainingQuestions = async(req,res,next) => {
     try {
-        const userId = (req.user ? req.user._id : (req.body.user ? req.body.user._id : undefined));
-        if(!userId) throw new Error("No user id provided!");
+        //const userId = (req.user ? req.user._id : (req.body.user ? req.body.user._id : undefined));
+        //if(!userId) throw new Error("No user id provided!");
 
-        if(!await User.findById(userId)) throw new Error(`No user exists with id: ${userId}!`);
+        const userId = req.user._id;
+        if(!await User.findById(userId)) {
+            throw new Error(`No user exists with id: ${userId}!`);
+        }
 
         const quiz = await Quiz.findOne({owner: userId});
-        if(!quiz) throw new Error(`user with id: ${userId} has no quiz!`);
+        if(!quiz) {
+            throw new Error(`user with id: ${userId} has no quiz!`);
+        }
 
         return res.status(200).json({
-            data: quiz.flashcards.length
+            data: quiz.numRemainingFlashcards
         })
     }
 
@@ -141,10 +156,14 @@ exports.validateQuestion = async(req,res,next) => {
     try {
         const questionId = req.params.questionId;
         const flashcard = await Flashcard.findById(questionId);
-        if(!flashcard) throw new Error(`Question with id: ${questionId} not found!`);
+        if(!flashcard) {
+            throw new Error(`Question with id: ${questionId} not found!`);
+        }
 
         const answer = req.body.answer;
-        if(!answer) throw new Error("No answer provided!");
+        if(!answer) {
+            throw new Error("No answer provided!");
+        }
 
         const isCorrect = (answer == flashcard.answer);
         flashcard.count = (isCorrect ? flashcard.count + 1 : 0);
