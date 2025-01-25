d0134650499f273da27d715dee72919cd8a2b743
PeterKrauza <peterkrauza1@gmail.com>
2025-01-25 17:28:57 +0100

    subject response model

---
+++ angular-client/src/models/response/subject-response.ts
@@ -0,0 +1,6 @@
+import ISubject from '../subject';
+
+export interface SubjectResponse{
+  message: string;
+  data: ISubject[];
+}
--- angular-client/src/services/subject-service/subject.service.ts
+++ angular-client/src/services/subject-service/subject.service.ts
@@ -2,6 +2,7 @@ import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import ISubject, {IDirectory} from '../../models/subject';
+import {SubjectResponse} from '../../models/response/subject-response';
 
 @Injectable({
   providedIn: 'root'
@@ -16,16 +17,11 @@ export class SubjectService {
       )
   }
   getSubjects() {
-    return this.http.get<{message: string, data: ISubject[]}>(`${environment.api}/subjects/`);
+    return this.http.get<SubjectResponse>(`${environment.api}/subjects/`);
   }
 
-  getDirectories(subjectId: string){
-    return this.http.get<{message: string, data: IDirectory[]}>(`${environment.api}/subjects/${subjectId}`)
-  }
-  // updateDirectories(subjectId: string, directories: string){
-  //   return this.http.put(`${environment.api}/subjects/${subjectId}`,{
-  //     params: { subjectId, directories }
-  //   })
+  // getDirectories(subjectId: string){
+  //   return this.http.get<{message: string, data: IDirectory[]}>(`${environment.api}/subjects/${subjectId}`)
   // }
   updateSubject(subjectId: string, subjectName: string, directories: IDirectory[]){
     return this.http.patch(`${environment.api}/subjects/${subjectId}`,{ subjectName, directories })
